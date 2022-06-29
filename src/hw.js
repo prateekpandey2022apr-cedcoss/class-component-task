0ce4b81f47171645146e8445ecf3e3c8304f3bda
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-06-29 11:35:43 +0530

    Do the same using arrow functions.

--- src/hw.js
+++ src/hw.js
@@ -1,11 +1,15 @@
 import React from "react";
 // import App from "./App.js";
 
-class HW extends React.Component {
-  render() {
-    return <h1>Hello world</h1>;
-  }
-}
+// class HW extends React.Component {
+//   render() {
+//     return <h1>Hello world</h1>;
+//   }
+// }
+
+const HW = () => {
+  return <h1>Hello World!</h1>;
+};
 
 // export default HW;
 export { HW };
