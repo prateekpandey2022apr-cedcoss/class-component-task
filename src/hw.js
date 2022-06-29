a8fcf20211516ba0284f8dee851d0e281944af1c
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-06-29 11:31:24 +0530

    Make a class component as a child of App.js and print Hello world. Try using named and default exports.

---
+++ src/hw.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import App from "./App.js";
+
+class HW extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+// export default HW;
+export { HW };
