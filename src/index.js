632242c70e75f872d7ca646856e9bacc01b32022
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-06-29 11:43:37 +0530

    Print the following data with two components MobileOS and MobileManufacturers using class components.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HW } from "./hw.js";
+// import { HW } from "./hw.js";?
+import { MobileOS, MobileManufacturers } from "./mobile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HW />
+    <MobileOS />
+    <MobileManufacturers />
   </React.StrictMode>
 );
 
